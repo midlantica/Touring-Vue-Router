38b0a199bb340b9dcb1b1d84d11a698d983807b3
midlantica <druharper@gmail.com>
2022-10-03 15:01:00 -0500

    save

---
+++ src/views/event/Register.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>Register the event here</p>
+  <button @click="register">Register Me</button>
+</template>
+
+<script>
+export default {
+  props: ['event'],
+  methods: {
+    register () {
+      // Call to API
+      // If registered then redirect to event to details
+
+      this.$router.push({
+        name: 'EventDetails',
+        // params: { id: this.event.id } // no need since populated by default
+      })
+    }
+  }
+
+}
+</script>
